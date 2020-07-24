0477e7085282c06e2024a2fd6df0537e153fabb2
Jeffry Hermanto <jeffryhermanto@gmail.com>
2020-07-24 14:49:14 +0700

    Refactor Testimonial Area

--- components/TestimonialArea.vue
+++ components/TestimonialArea.vue
@@ -7,56 +7,16 @@
           <!-- start testimonial carousel -->
           <div class="test-slider">
             <!-- start tetimonial item -->
-            <div class="test-item">
-              <p>Having supervised Jeffry Hermanto for the last few months, I have witnessed his concerted efforts to improve the daily performance of his tasks, and I have observed his desire to meet the demands and expectations of his co-workers and environment. Given the integrity and strength of commitment demonstrated by his past performance, I would like to recommend Jeffry Hermanto as a suitable candidate for the position of Mobile Engineer at your company.</p>
+            <div v-for="(author, i) in authors" :key="i" class="test-item">
+              <p>{{ author.testimony }}</p>
               <div class="author-info">
-                <img src="assets/img/testimonial/marcelo.jpg" alt="client" />
-                <h4>Marcelo Milhomem</h4>
+                <img :src="`assets/img/testimonial/${author.image}`" alt="client" />
+                <h4>{{ author.name }}</h4>
                 <div class="position">
-                  CTO
-                  <span>Loadsmile</span>
-                </div>
-              </div>
-            </div>
-            <!-- end tetimonial item -->
-
-            <!-- start tetimonial item -->
-            <div class="test-item">
-              <p>I have worked with Jeffry on two complex projects and in both cases, the work was done on time, on budget, and with a high degree of professionalism. Across the board, everyone enjoyed working with Jeffry. Jeffry not only did exceptional work based on the specs we gave him for each project, but he also gave input on how to make the end product even better and cared about how users would engage with the builds. I will be using Jeffry again in the future and would highly recommend his services to anyone.</p>
-              <div class="author-info">
-                <img src="assets/img/testimonial/antakirana.jpg" alt="client" />
-                <h4>Antakirana</h4>
-                <div class="position">
-                  Lead Pastor
-                  <span>MCC BSD</span>
-                </div>
-              </div>
-            </div>
-            <!-- end tetimonial item -->
-
-            <!-- start tetimonial item -->
-            <div class="test-item">
-              <p>I rarely come across real talents who stand out like Jeffry. I had the pleasure of working with Jeffry for 1 years at Sprout Digital Labs, collaborating on several project teams. Jeffry's ability to handle multiple projects was unlike any I’ve seen before and made a dramatic increase in the productivity level of our company. No matter how tense a meeting, Jeffry made sure everyone left with a smile. As a team member or a leader, Jeffry earns my highest recommendation.</p>
-              <div class="author-info">
-                <img src="assets/img/testimonial/sari.jpg" alt="client" />
-                <h4>Sari Harasti</h4>
-                <div class="position">
-                  Product Manager Manager
-                  <span>PurpleRain Technologies Inc.</span>
-                </div>
-              </div>
-            </div>
-            <!-- end tetimonial item -->
-
-            <!-- start tetimonial item -->
-            <div class="test-item">
-              <p>Jeffry's talent and experience proved immensely valuable for our latest web project. He was involved from the very beginning, providing advice and guidance even before we signed the proposal. The work we received from him was top-notch, thoroughly documented, and full of touches that show his commitment to quality. I can’t recommend him highly enough.</p>
-              <div class="author-info">
-                <img src="assets/img/testimonial/adirani.jpg" alt="client" />
-                <h4>Adirani Heraputeri</h4>
-                <div class="position">
-                  Project Manager
-                  <span>ANGIN</span>
+                  {{ author.position }}
+                  <span>
+                    <a :href="author.url" target="_blank">{{ author.company }}</a>
+                  </span>
                 </div>
               </div>
             </div>
@@ -71,7 +31,14 @@
 </template>
 
 <script>
+import authors from "~/json/authors.json";
+
 export default {
-  name: "TestimonialArea"
+  name: "TestimonialArea",
+  data() {
+    return {
+      authors,
+    };
+  },
 };
 </script>
