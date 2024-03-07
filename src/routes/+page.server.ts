dc649003163f2aada618b73f2d3992b3c0533ea8
Jeffry Hermanto <jeffryhermanto@gmail.com>
2024-03-07 08:56:49 +0700

    Reword root page title

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,7 @@
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = () => {
-	const title = 'Jeffry Hermanto - Software Engineer whom Clients LOVE to Hire';
+	const title = 'Jeffry Hermanto - Software Engineer that Clients Love to Hire';
 
 	const description =
 		'Experienced full-stack web & mobile developer who has a track record of success creating apps & websites that are both well-received & commercially viable. Skilled with working as a team & incorporating input into projects. Ability to always look for ways to improve upon an already existing app to keep people downloading it & enjoying it. Strong eye for detail & tenacity to never quit on something until it is absolutely perfect.';
