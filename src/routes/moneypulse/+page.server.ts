02889e9325d1656db066e248d5c9ddaaba69ffa9
Jeffry Hermanto <jeffryhermanto@gmail.com>
2025-02-14 17:53:33 +0700

    MoneyPulse: Shortened download label

--- src/routes/moneypulse/+page.server.ts
+++ src/routes/moneypulse/+page.server.ts
@@ -5,34 +5,35 @@ export const load: PageServerLoad = () => {
 
 	const downloadFiles = [
 		{
-			label: 'Penghasilan 5 juta per bulan',
+			label: '5 juta / bulan',
 			path: `${sourcePath}/5jt_moneypulse_template.mpd`,
 			fileName: '5jt_moneypulse_template.mpd'
 		},
 		{
-			label: 'Penghasilan 10 juta per bulan',
+			label: '10 juta / bulan',
 			path: `${sourcePath}/10jt_moneypulse_template.mpd`,
 			fileName: '10jt_moneypulse_template.mpd'
 		},
 		{
-			label: 'Penghasilan 15 juta per bulan',
+			label: '15 juta / bulan',
 			path: `${sourcePath}/15jt_moneypulse_template.mpd`,
 			fileName: '15jt_moneypulse_template.mpd'
 		},
 		{
-			label: 'Penghasilan 30 juta per bulan',
+			label: '30 juta / bulan',
 			path: `${sourcePath}/30jt_moneypulse_template.mpd`,
 			fileName: '30jt_moneypulse_template.mpd'
 		},
+
 		{
-			label: 'Penghasilan US $10,000 per bulan',
-			path: `${sourcePath}/$10k_moneypulse_template.mpd`,
-			fileName: '$10k_moneypulse_template.mpd'
-		},
-		{
-			label: 'Penghasilan Dengan Berbagai Sumber',
+			label: 'Multi Income',
 			path: `${sourcePath}/multi_income_moneypulse_template.mpd`,
 			fileName: 'multi_income_moneypulse_template.mpd'
+		},
+		{
+			label: 'US $10,000 monthly',
+			path: `${sourcePath}/$10k_moneypulse_template.mpd`,
+			fileName: '$10k_moneypulse_template.mpd'
 		}
 	];
 
