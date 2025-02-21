ee71fde8f4f569a1647325eacfcbb55e0fe0b7c8
Jeffry Hermanto <jeffryhermanto@gmail.com>
2025-02-21 10:14:01 +0700

    Update CV

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,7 +4,7 @@ export const load: PageServerLoad = () => {
 	const title = 'Jeffry Hermanto - Software Engineer that Clients Love to Hire';
 
 	const description =
-		'Experienced full-stack web & mobile developer who has a track record of success creating apps & websites that are both well-received & commercially viable. Skilled with working as a team & incorporating input into projects. Ability to always look for ways to improve upon an already existing app to keep people downloading it & enjoying it. Strong eye for detail & tenacity to never quit on something until it is absolutely perfect.';
+		'Experienced full-stack web and mobile developer with a proven track record of creating successful, commercially viable apps and websites. Skilled in teamwork and incorporating feedback into projects. Consistently seeks ways to improve existing apps, ensuring continued user engagement and satisfaction. Strong attention to detail and the tenacity to perfect every project.';
 
 	const author = 'Jeffry Hermanto';
 
