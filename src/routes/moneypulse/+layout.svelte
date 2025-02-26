a231f7b056a393a153054521b77f494ca5af3e33
Jeffry Hermanto <jeffryhermanto@gmail.com>
2025-02-26 17:44:56 +0700

    fix(moneypulse): Replace jquery script src

--- src/routes/moneypulse/+layout.svelte
+++ src/routes/moneypulse/+layout.svelte
@@ -7,19 +7,12 @@
 	<title>MoneyPulse</title>
 	<meta name="description" content="Financial Checkup App" />
 	<meta name="author" content="Jeffry Hermanto" />
-
-	<script
-		src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-		integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-		crossorigin="anonymous"
-	></script>
-
 	<link
 		href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
 		rel="stylesheet"
 	/>
-
 	<link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </svelte:head>
 
